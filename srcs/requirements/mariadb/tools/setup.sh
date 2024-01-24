c30a317b8582625b6827bab27277149aff721581
marcosviudes <mviudes@student.42madrid.com>
2024-01-24 17:06:10 +0100

    Entregado

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -13,20 +13,12 @@ sleep 3
 if [ ! -d "/var/lib/mysql/${MYSQL_DATABASE_NAME}" ]; then
     mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE_NAME;"
     mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+    mysql -e "CREATE USER IF NOT EXISTS $MYSQL_USER@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
+    mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE_NAME.* TO $MYSQL_USER@'localhost' WITH GRANT OPTION;"
     mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%';"
     mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
     mysql -e "FLUSH PRIVILEGES;"
 
 fi
 /etc/init.d/mariadb stop
-mysqld_safe
-#sleep infinity
-
-#mysql -e "CREATE USER IF NOT EXISTS $MYSQL_USER@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
-#mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE_NAME.* TO $MYSQL_USER@'localhost' WITH GRANT OPTION;"
-
-#mysql -uroot -e "DROP USER 'root'@'localhost';"
-#mysql -uroot -e "CREATE USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-#mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;"
-#mysql -e "FLUSH PRIVILEGES;"
-#exec mysqld_safe --console
+mysqld_safe 
--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 
-#sleep 15
-
 until nc -z -v -w30 $MYSQL_DATABASE_HOST 3306
 do
   echo "Esperando a que la base de datos esté disponible..."
   sleep 5
 done
-
-
-#chown -R www-data:www-data /var/www/wordpress
-#chmod -R 755 /var/www/wordpress
 
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
@@ -27,15 +21,12 @@ if [ ! -e /var/www/wordpress/wp-config.php ]; then
 fi
 
 if [ ! -e /var/www/wordpress/user_created.txt ]; then
-  #wp user create $WP_ADMIN $WP_ADMIN_MAIL --role=administrator --user_pass=$WP_ADMIN_PASSWORD --path=var/www/wordpress
   touch /var/www/wordpress/user_created.txt
 fi
 
 if [ ! -e /var/www/wordpress/home_page_created.txt ]; then
-  #wp post create --post_type=page --post_title='Inception' --post_status=publish --path=var/www/wordpress
   wp core install  --url=$DOMAIN_NAME --title=Inception --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email="$WP_ADMIN_MAIL" --path=/var/www/wordpress
   wp user create $WP_USER $WP_USER_MAIL --role=editor --user_pass=$WP_USER_PASSWORD --path=/var/www/wordpress
-  #wp theme install --activate twentyseventeen --path=/var/www/wordpress
   touch /var/www/wordpress/home_page_created.txt
 fi
 
